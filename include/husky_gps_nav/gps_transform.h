f2ddecfd99bb9ed844eba17b00104ac7b974a04f
Jake Keller <jakeller3630@gmail.com>
2021-06-24 16:43:28 -0400

    Finish fromLL server wrapper

--- include/husky_gps_nav/gps_transform.h
+++ include/husky_gps_nav/gps_transform.h
@@ -2,6 +2,7 @@
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geographic_msgs/GeoPoint.h"
+#include "robot_localization/FromLL.h"
 
 
 class GPSTransform
@@ -13,6 +14,9 @@ private:
     void setpointCallback(const geographic_msgs::GeoPoint::ConstPtr& msg);
     ros::NodeHandle nh_;
     ros::Subscriber setpoint_sub_;
+    ros::ServiceClient from_ll_client_;
+    robot_localization::FromLL from_ll_srv_;
+    
 };
 
 
@@ -22,6 +26,7 @@ GPSTransform::GPSTransform(ros::NodeHandle &nh) : nh_(nh)
                                   10,
                                   &GPSTransform::setpointCallback,
                                   this);
+    from_ll_client_ = nh_.serviceClient<robot_localization::FromLL>("fromLL");
 
 }
 
@@ -29,5 +34,15 @@ void GPSTransform::setpointCallback(const geographic_msgs::GeoPoint::ConstPtr& m
 {
     ROS_INFO("Recieved setpoint, lat: %f long: %f alt: %f",
              msg->latitude, msg->longitude, msg->altitude);
+    from_ll_srv_.request.ll_point = *msg;
+    if (from_ll_client_.call(from_ll_srv_)) {
+        ROS_INFO("Transformed to map point, x: %f y: %f z: %f",
+                 from_ll_srv_.response.map_point.x,
+                 from_ll_srv_.response.map_point.y,
+                 from_ll_srv_.response.map_point.z);
+    }
+    else {
+        ROS_ERROR("Failed to call service fromLL");
+    }
 }
 
