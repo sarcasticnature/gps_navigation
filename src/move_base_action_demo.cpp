adafb4da0893e9b123a7be09075758fb63406e83
Jake Keller <jakeller3630@gmail.com>
2021-07-16 11:16:53 -0400

    Change action demo to use a class & feedback

--- src/move_base_action_demo.cpp
+++ src/move_base_action_demo.cpp
@@ -2,49 +2,63 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
+using Client = actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>;
 
-void doneCallback(const actionlib::SimpleClientGoalState& state,
-                  const move_base_msgs::MoveBaseResultConstPtr& result)
+class DriveActionClient
 {
-  ROS_INFO("Finished move_base action");
-  if(state == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Successfully moved 1 meter forward");
-  else
-    ROS_INFO("Failed to move forward for an unknown reason");
-}
+public:
+  explicit DriveActionClient(ros::NodeHandle nh) : ac_("move_base", true),
+                                                   nh_(nh)
+  {
+    ROS_INFO("Waiting for server");
+    ac_.waitForServer();
+    ROS_INFO("Server started");
+  }
+
+  void doneCallback(const actionlib::SimpleClientGoalState& state,
+                    const move_base_msgs::MoveBaseResultConstPtr& result)
+  {
+
+
+  }
+
+  void feedbackCallback(const move_base_msgs::MoveBaseFeedbackConstPtr& fb)
+  {
+    ROS_INFO_STREAM("Feedback: x=" << fb->base_position.pose.position.x
+                    << " y=" << fb->base_position.pose.position.y);
+  }
+
+  void driveForward(int distance)
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
+  
+    goal.target_pose.pose.position.x = distance;
+    goal.target_pose.pose.orientation.w = 1.0;
+
+    ac_.sendGoal(goal,
+                 Client::SimpleDoneCallback(),
+                 Client::SimpleActiveCallback(),
+                 boost::bind(&DriveActionClient::feedbackCallback, this, _1));
+  }
+
+private:
+  ros::NodeHandle nh_;
+  Client ac_;
+};
+
 
 int main(int argc, char** argv)
 {
-  using MoveBaseClient = actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>;
 
   ros::init(argc, argv, "move_base_action_demo");
+  ros::NodeHandle nh;
 
-  MoveBaseClient ac("move_base", true);
+  DriveActionClient drive_ac(nh);
 
-  while(!ac.waitForServer(ros::Duration(5.0))) {
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-
-  move_base_msgs::MoveBaseGoal goal;
-
-  goal.target_pose.header.frame_id = "base_link";
-  goal.target_pose.header.stamp = ros::Time::now();
-
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
-
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal, &doneCallback);
-
+  drive_ac.driveForward(1.5);
   ros::spin();
-
-//  ac.waitForResult();
-//
-//  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-//    ROS_INFO("Successfully moved 1 meter forward");
-//  else
-//    ROS_INFO("Failed to move forward for an unknown reason");
-
   return 0;
 }
 
