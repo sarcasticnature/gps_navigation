eaac6f6d62dc21a30f23f090751bc6eedce83d17
Jake Keller <jakeller3630@gmail.com>
2021-07-28 14:40:06 -0400

    Update waypoint_printer to read data into a vector

--- include/gps_navigation/waypoint_printer.h
+++ include/gps_navigation/waypoint_printer.h
@@ -1,6 +1,8 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <utility>
 
 #include "ros/ros.h"
 #include "ros/package.h"
@@ -8,18 +10,32 @@
 namespace gps_navigation
 {
 
+struct Waypoint
+{
+  double lat;
+  double lon;
+};
+
 class WaypointPrinter
 {
 public:
+  struct Waypoint
+  {
+    double lat;
+    double lon;
+  };
+
   explicit WaypointPrinter(ros::NodeHandle nh);
 
 
 private:
+  void readFile();
   void printFile();
 
   ros::NodeHandle nh_;
   std::string filename_;
   std::ifstream waypoint_file_;
+  std::vector<Waypoint> waypoints_;
 
 };
 
@@ -33,19 +49,32 @@ WaypointPrinter::WaypointPrinter(ros::NodeHandle nh) : nh_(nh)
   ROS_INFO_STREAM("Full path is: " << ros::package::getPath("gps_navigation")
                       + "/waypoint_files/"
                       + filename_);
+  readFile();
   printFile();
   ros::shutdown();
 }
 
+void WaypointPrinter::readFile()
+{
+  Waypoint point;
+  std::string datum;
+  while (std::getline(waypoint_file_, datum, ',')) {
+    point.lat = stof(datum);
+    std::getline(waypoint_file_, datum, '\n');
+    point.lon = stof(datum);
+
+    waypoints_.push_back(point);
+  }
+
+
+
+}
+
 void WaypointPrinter::printFile()
 {
-  std::string lat;
-  std::string lon;
-  while (std::getline(waypoint_file_, lat, ',')) {
-    std::getline(waypoint_file_, lon, '\n');
-    ROS_INFO_STREAM("Read waypoint: latitude: " << lat
-                    << " longitude: " << lon);
-
+  ROS_INFO_STREAM("Read waypoints:");
+  for (auto point : waypoints_) {
+    ROS_INFO_STREAM(std::fixed << std::setprecision(8) << "Lat: " << point.lat << " Long: " << point.lon);
   }
 
 }
